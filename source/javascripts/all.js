462d7e4e09e1cd3ced7966ee7a07e116884b5692
Scott Anderson <scott@pagodabox.com>
2016-06-29 15:34:01 -0600

    built the homepage for the guides

---
+++ source/javascripts/all.js
@@ -0,0 +1,46 @@
+//= require_tree .
+//= require _jquery-3.0.0.min.js
+
+
+$(document).ready(function() {
+
+  ///////////////////// HOME PAGE PROJECT SEARCH SHOW/HIDE /////////////////////
+
+  $('input#project-search').keyup(function(){
+
+    var string = $('input#project-search').val().toLowerCase();
+    var matched = $( "a.project[search-tags*='" + string + "']" )
+    var unmatched = $( "a.project").not("[search-tags*='" + string + "']" )
+
+    // Show/Hide Matched Projects
+    if ($('input#project-search').val() ) {
+      $(matched).show(200);
+      $(unmatched).hide(200);
+    } else {
+      $('a.project').show(200);
+    }
+
+    // Show/Hide Categories
+    $('.category').each(function () {
+      if ( $('input#project-search').val() === '' && $(this).not(':visible') ) {
+        $(this).fadeIn(400);
+      }
+      else if ($(this).has(matched).length > 0 ) {
+        $(this).fadeIn(400);
+      }
+      else if ($(this).has(matched).length === 0 ) {
+        $(this).hide(200);
+      }
+    })
+
+    // Show/Hide No Results
+    if ( matched.length > 0 || $('input#project-search').val() === '') {
+      $('#no-results').fadeOut(200)
+    }
+    if ( matched.length === 0 &&  $('input#project-search').val() != '') {
+      $('#no-results').fadeIn(1000);
+    }
+
+
+  })
+})
